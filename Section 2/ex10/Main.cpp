b38955e9b8f26c2989cbf97c1544ff0e584a37e9
David <109089386+DvdHalliday@users.noreply.github.com>
2022-07-11 15:50:37 -0500

    Last commit!!

--- Section 2/ex10/Main.cpp
+++ Section 2/ex10/Main.cpp
@@ -1,5 +1,11 @@
 #include <iostream>
 
 int main() {
-
+	char input;
+	std::cout << "Hello! Please enter an alphabetical character: ";
+	std::cin >> input;
+	std::string response = (input != 97 && input != 101 && input != 105 && input != 111 && input != 117)
+		? "Your character is indeed NOT a lowercase vowel!!"
+		: "Your character IS a lowercase vowel ;c";
+	std::cout << response << "\n";
 }
--- Section 2/ex9/Main.cpp
+++ Section 2/ex9/Main.cpp
@@ -1,5 +1,11 @@
 #include <iostream>
 
 int main() {
-
+	char input;
+	std::cout << "Hello! Please enter an alphabetical character: ";
+	std::cin >> input;
+	std::string response = (input == 97 || input == 101 || input == 105 || input == 111 || input == 117)
+		? "Your character is a lowercase vowel!"
+		: "Your character is NOT a lowercase vowel.";
+	std::cout << response << "\n";
 }
