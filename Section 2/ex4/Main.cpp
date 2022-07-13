e5f912881948aa2d6234becf67ff69cd47a6d4b5
David <109089386+DvdHalliday@users.noreply.github.com>
2022-07-13 08:42:45 -0500

    Final update ig

--- Section 2/ex4/Main.cpp
+++ Section 2/ex4/Main.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 
-int getAwardedPointsFromAnswer(bool isCorrectAnswer) {
-	std::string response = (isCorrectAnswer) ? "CORRECT! Looks like you have common sense." : "WRONG! How could you miss that??";
+int getAwardedPointsFromAnswer(bool isCorrectAnswer, bool actualAnswer) {
+	std::string response = (isCorrectAnswer == actualAnswer) ? "CORRECT! Looks like you have common sense." : "WRONG! How could you miss that??";
 	std::cout << response << "\n";
-	return (isCorrectAnswer) ? 1 : 0;
+	return (isCorrectAnswer == actualAnswer) ? 1 : 0;
 }
 
 int main() {
@@ -11,12 +11,12 @@ int main() {
 	std::cout << "Welcome to your quiz!\n0 - False\n1 - True\n\nQuestion 1: Are avocados fruits ? ";
 	bool firstAnswer, secondAnswer, thirdAnswer;
 	std::cin >> firstAnswer;
-	grade += getAwardedPointsFromAnswer(firstAnswer);
+	grade += getAwardedPointsFromAnswer(firstAnswer, 1);
 	std::cout << "Question 2: Is the sky blue? ";
 	std::cin >> secondAnswer;
-	grade += getAwardedPointsFromAnswer(secondAnswer);
+	grade += getAwardedPointsFromAnswer(secondAnswer, 1);
 	std::cout << "Question 3: Is Teravision the Best.Team.Ever? ";
 	std::cin >> thirdAnswer;
-	grade += getAwardedPointsFromAnswer(thirdAnswer);
+	grade += getAwardedPointsFromAnswer(thirdAnswer, 1);
 	std::cout << "You got " << grade << "/3 correct answers! Your final grade is: " << grade*5 / 3.0f << "\n";
 }
